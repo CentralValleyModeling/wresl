b6a49f34f53e488134df101e1d4f51007e0b9483
Zachary Roy <zachary.roy@water.ca.gov>
2026-06-09 08:20:55 -0700

    Add new problem with more constraints and variables

--- docs/plots.py
+++ docs/plots.py
@@ -7,7 +7,7 @@ LOGGER = get_logger(__name__)
 
 
 def tutorials_index_01():
-    problem: pulp.LpProblem = pulp.LpProblem("Mass-Balance-Example", pulp.LpMaximize)
+    problem: pulp.LpProblem = pulp.LpProblem("tutorials_index_01", pulp.LpMaximize)
     # Constraints
     OUTFLOW = problem.add_variable("OUTFLOW", 0, None)
     DELIVERY = problem.add_variable("DELIVERY", 0, 50)
@@ -24,3 +24,32 @@ def tutorials_index_01():
     # Plot things
     plot_problem("DELIVERY", "OUTFLOW", problem=problem)
     plt.show()
+
+
+def tutorials_index_02():
+    problem: pulp.LpProblem = pulp.LpProblem("tutorials_index_02", pulp.LpMaximize)
+    # Constraints
+    OUTFLOW = problem.add_variable("OUTFLOW", 0, None)
+    DELIVERY = problem.add_variable("DELIVERY", 0, 50)
+    UNMET_DEMAND = problem.add_variable("UNMET_DEMAND", 0, None)
+    demand = 100
+    # Objective
+    problem += (10 * DELIVERY) + (1 * OUTFLOW) + (-1000 * UNMET_DEMAND)
+
+    # Constraints
+    problem += OUTFLOW >= (0.25 * DELIVERY) + 25, "MINIMUM_FLOW_REQUIREMENT"
+    problem += 60 - OUTFLOW - DELIVERY == 0, "MASS_BALANCE"
+    problem += demand - DELIVERY - UNMET_DEMAND == 0, "DEMAND"
+
+    # Solve so we can plot the optimal point
+    LOGGER.info("solving pulp problem".center(50, "."))
+    problem.solve(solver=pulp.getSolver("PULP_CBC_CMD"))
+    LOGGER.info("pulp problem done solving".center(50, "."))
+
+    # Plot things
+    plot_problem("DELIVERY", "UNMET_DEMAND", problem=problem)
+    plt.show()
+
+
+if __name__ == "__main__":
+    tutorials_index_02()
